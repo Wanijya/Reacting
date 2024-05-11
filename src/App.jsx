5d9ed08960e163777742e709e3499e864ea29152
Wanijya <wanijyabisen@gmail.com>
2024-05-11 18:51:45 +0530

    useState()

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-import Card from "./components/Card";
+import React, { useState } from "react";
 const App = () => {
+
+  const [val, setVal] = useState({name: "Wani", isBanned: false});
+
   return (
-    <div>
-      <Card />
+    <div className="m-5">
+      <h1>name: {val.name}</h1>
+      <h2>isBanned: {val.isBanned.toString()}</h2>
+      <button onClick={()=>setVal({...val, isBanned: !val.isBanned})} className={`px-3 py-1 ${val.isBanned ? "bg-blue-500" : "bg-red-500"} rounded-md`}>Change kro</button>
     </div>
   );
 };
